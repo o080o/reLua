7ceb4353a2cfc501253ba6d32ed47ce19f1fc228
Kenny MacDermid <kenny.macdermid@gmail.com>
2018-12-06 16:19:14 -0400

    Add a busted test spec for regexes.

---
+++ re_spec.lua
@@ -0,0 +1,33 @@
+local re = require("re")
+local say = require("say")
+
+-- Set up a regex matching assert:
+local function regex_matches(state, arguments)
+	local regex = arguments[1]
+	local str = arguments[2]
+	return re.compile(regex):execute(str) ~= nil
+end
+
+say:set_namespace("en")
+say:set("assertion.regex_matches.positive", "Expected regex %s to match: %s")
+say:set("assertion.regex_matches.negative",
+	"Expected regex %s to not match: %s")
+assert:register("assertion", "regex_matches", regex_matches,
+	"assertion.regex_matches.positive", "assertion.regex_matches.negative")
+
+
+describe("re execute", function()
+	it("should match simple regexes", function()
+		assert.regex_matches("abc", "abc")
+	end)
+
+	it("shouldn't match a simple non-match", function()
+		assert.not_regex_matches("abc", "def")
+	end)
+
+	describe("dot chars", function()
+		it("should match any char", function()
+			assert.regex_matches("...", "abc")
+		end)
+  end)
+end)
